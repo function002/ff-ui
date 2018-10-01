bb8fbfedd1883b2e38d1a32c71b05d431a5f7c0d
function002 <functionfeeders@gmail.com>
2018-10-01 10:06:13 +0530

    Added home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
-import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
 const routes: Routes = [
-  { path: '', component: HeaderComponent },
-  { path: '***', component: HeaderComponent }
+  { path: '', component: HomeComponent },
+  { path: '***', component: HomeComponent }
 ];
 @NgModule({
   imports: [ RouterModule.forRoot(routes) ],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
